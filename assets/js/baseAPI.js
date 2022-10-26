32db0aaca808a728b58d6bf1643c9531a4a2219c
‘LangTu6’ <1656537797@qq.com>
2022-10-26 11:59:39 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,25 @@ $.ajaxPrefilter(function(options) {
     // console.log(options.url);
     options.url = 'http://www.liulongbin.top:3007' + options.url
         // console.log(options.url);
+
+    //统一为有权限的接口，设置headers请求头
+    //-1 表示“未找到匹配项”
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载complete 回调函数
+    options.complete = function(res) {
+        console.log('执行了');
+        // console.log(res);
+        //在complete回调函数中,可以使用res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.强制清空token
+            localStorage.removeItem('token')
+                //2.强制跳转登录页面
+            location.href = '/login.html'
+        }
+    }
 })
