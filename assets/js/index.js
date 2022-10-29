704768c56d215d0097d582a5ba3c7fd5f00b7af3
‘LangTu6’ <1656537797@qq.com>
2022-10-29 23:43:11 +0800

    完成了个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ $(function() {
 
                 //关闭confim询问框
                 layer.close(index);
-            });
+            })
         })
     })
     //获取用户的基本信息
@@ -31,24 +31,24 @@ function getUserInfo() {
         //     Authorization: localStorage.getItem('token') || ''
         // },
         success: function(res) {
-            if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败')
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败')
+                }
+                //调用renderAvatar渲染用户的头像
+                renderAvatar(res.data)
             }
-            //调用renderAvatar渲染用户的头像
-            renderAvatar(res.data)
-        },
-        // //不论成功还是失败,最终都会调用complete函数
-        // complete: function(res) {
-        //     // console.log('执行了');
-        //     // console.log(res);
-        //     //在complete回调函数中,可以使用res.responseJSON 拿到服务器响应回来的数据
-        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-        //         //1.强制清空token
-        //         localStorage.removeItem('token')
-        //             //2.强制跳转登录页面
-        //         location.href = '/login.html'
-        //     }
-        // }
+            // //不论成功还是失败,最终都会调用complete函数
+            // complete: function(res) {
+            //     // console.log('执行了');
+            //     // console.log(res);
+            //     //在complete回调函数中,可以使用res.responseJSON 拿到服务器响应回来的数据
+            //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //         //1.强制清空token
+            //         localStorage.removeItem('token')
+            //             //2.强制跳转登录页面
+            //         location.href = '/login.html'
+            //     }
+            // }
     })
 }
 //渲染用户的头像
@@ -60,7 +60,7 @@ function renderAvatar(user) {
         ///3.按需渲染用户的头像
     if (user.user_pic !== null) {
         //3.1渲染图片头像
-        $('.layui-nav-img').attr('src', user._pic).show()
+        $('.layui-nav-img').attr('src', user.user_pic).show()
         $('.text-avatar').hide()
     } else {
         //3.2渲染文本头像
